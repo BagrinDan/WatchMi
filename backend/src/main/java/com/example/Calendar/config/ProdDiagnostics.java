de876ba3e63b3b2c8934281fa38f5b9b07ab6e8b
BagrinD <bagrin.danu@gmail.com>
2026-03-09 21:26:54 +0200

    Corrected docker and .env files

--- backend/src/main/java/com/example/Calendar/config/ProdDiagnostics.java
+++ backend/src/main/java/com/example/Calendar/config/ProdDiagnostics.java
@@ -1,10 +1,12 @@
 package com.example.Calendar.config;
 
 
+import org.hibernate.cfg.Environment;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+
 
 @Configuration
 @Profile("prod")
@@ -13,4 +15,5 @@ public class ProdDiagnostics {
     public CommandLineRunner run(){
         return args -> System.out.println("[***] Profile: PROD");
     }
+
 }
